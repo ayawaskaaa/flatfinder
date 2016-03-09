6dc36771e617b7852b00f8c909e9026bd22bd89b
Vasiliy Chumak <Vasiliy Chumak>
2016-03-10 00:55:08 +0300

    Optimize bootcontroler to work without store

--- src/main/java/com/flat/controller/BootController.java
+++ src/main/java/com/flat/controller/BootController.java
@@ -2,15 +2,15 @@ package com.flat.controller;
 
 import com.flat.model.Apartment;
 import com.flat.utils.ApiCall;
-import com.flat.utils.CollectionUtils;
+import com.flat.utils.Cache;
 import com.flat.utils.Notificator;
-import com.flat.utils.Store;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,7 +24,7 @@ public class BootController
 	ApiCall apiCall;
 
 	@Autowired
-	Store store;
+	Cache cache;
 
 	@Autowired
 	Notificator notificator;
@@ -34,11 +34,19 @@ public class BootController
 	public void refresh() {
 		try {
 			List<Apartment> apartments = apiCall.doCall();
-			List<Long> existingApartmentIds = store.getLastApartmentsIds();
+			if(!cache.isInitialized())
+			{
+				LOGGER.warning("Cache haven't been initialized, refresh it with new apartments");
+				cache.setApartments(apartments);
+				return;
+			}
+			List<Apartment> existingApartments = cache.getApartments();
 
-			List<Apartment> newApartments = CollectionUtils.getNewApartments(apartments, existingApartmentIds);
+			List<Apartment> newApartments = new ArrayList<>(apartments);
+			newApartments.removeAll(existingApartments);
+
 			if (!newApartments.isEmpty()) {
-				store.save(newApartments);
+				cache.setApartments(apartments);
 				notificator.notify(newApartments);
 			}
 		} catch (Throwable e) {
