d2a096503e3df213eb6a836b5c95bd7eb33f9527
Vasiliy Chumak <Vasiliy Chumak>
2016-03-10 00:55:46 +0300

    Format time in Minsk timezone for messages

--- src/main/java/com/flat/utils/MessageBuilder.java
+++ src/main/java/com/flat/utils/MessageBuilder.java
@@ -2,9 +2,13 @@ package com.flat.utils;
 
 import com.flat.model.Apartment;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.TimeZone;
 
 public class MessageBuilder {
+    public static DateFormat MINSK_DATE_FORMAT = createMinskDateFormatter();
 
     public static String buildApartmentsMessage(List<Apartment> apartments)
     {
@@ -34,7 +38,7 @@ public class MessageBuilder {
         builder.append('\n');
 
         builder.append("Last Updated - ");
-        builder.append(apartment.getLast_time_up());
+        builder.append(MINSK_DATE_FORMAT.format(apartment.getLast_time_up()));
         builder.append('\n');
 
         builder.append("Link - ");
@@ -42,5 +46,12 @@ public class MessageBuilder {
         builder.append('\n');
 
         return builder.toString();
+    }
+
+    private static DateFormat createMinskDateFormatter()
+    {
+        DateFormat formatter= new SimpleDateFormat("dd MMM yyyy HH:mm:ss");
+        formatter.setTimeZone(TimeZone.getTimeZone("Europe/Minsk"));
+        return formatter;
     }
 }
